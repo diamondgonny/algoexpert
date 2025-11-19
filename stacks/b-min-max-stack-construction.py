107c8f3cb50d39f384e50b242a830f058cd5e5aa
Kim Keon-Young <diamondgonny@gmail.com>
2025-11-19 20:42:54 +0900

    Solved

---
+++ stacks/b-min-max-stack-construction.py
@@ -0,0 +1,28 @@
+# 문제 풀이 1
+# Feel free to add new properties and methods to the class.
+class MinMaxStack:
+    def __init__(self):
+        self.minMaxStack = []
+        self.stack = []
+
+    def peek(self):
+        return self.stack[-1]
+
+    def pop(self):
+        self.minMaxStack.pop()
+        return self.stack.pop()
+
+    def push(self, number):
+        newMinMax = {"min": number, "max": number}
+        if len(self.minMaxStack):
+            lastMinMax = self.minMaxStack[-1]
+            newMinMax["min"] = min(lastMinMax["min"], number)
+            newMinMax["max"] = max(lastMinMax["max"], number)
+        self.minMaxStack.append(newMinMax)
+        self.stack.append(number)
+
+    def getMin(self):
+        return self.minMaxStack[-1]["min"]
+
+    def getMax(self):
+        return self.minMaxStack[-1]["max"]
